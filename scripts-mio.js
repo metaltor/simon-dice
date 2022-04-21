82c9c86348150d6e9ae3a824b57fd3fc798e1c6f
metaltor <gtn.280693@gmail.com>
2022-04-21 03:05:53 -0500

    funcionalidad al simon dice

--- scripts-mio.js
+++ scripts-mio.js
@@ -4,16 +4,78 @@ let secuenciaUsuario = [];
 let ronda=0;
 document.querySelector("button[type=button]").onclick = comenzarJuego
 
-function comenzarJuego (){
-   // resaltar()
-    obtenerCuadroAleatorio()
-    
+actualizarBarraEstado('Para empezar a jugar presiona el boton "Empezar"')
+actualizarNumeroRonda('-')
+bloquearClickUsuario()
+
+function comenzarJuego (){ 
+    reiniciarPartida()
+    actualizarBarraEstado('Turno de la maquina')
+    manejarRonda()
 }
+
+function reiniciarPartida(){
+    secuenciaMaquina=[];
+    secuenciaUsuario=[];
+    ronda=0
+};
 
 function manejarRonda (){
-secuenciaUsuario.forEach($cuadro)
-}
+    const $nuevoCuadro = obtenerCuadroAleatorio()
+    secuenciaMaquina.push($nuevoCuadro)
 
+secuenciaMaquina.forEach(function($cuadro,index){
+    const RETRASO_SM = (index + 1)*1000
+    setTimeout(function(){
+        resaltar($cuadro);
+    },RETRASO_SM);
+    
+});
+    const RETRASO_JUGADOR = (secuenciaMaquina.length+1) *1000
+    setTimeout(function(){
+        actualizarBarraEstado('Turno del Jugador');
+        DesbloquearClickUsuario();
+    }, RETRASO_JUGADOR)
+
+    ronda++
+    secuenciaUsuario=[]
+    actualizarNumeroRonda(ronda)
+};
+
+function manejarInputUsuario(e){
+    const $cuadro = e.target;
+    resaltar($cuadro)
+    secuenciaUsuario.push($cuadro);
+    const $cuadroMaquina = secuenciaMaquina[secuenciaUsuario.length-1]
+
+    if($cuadro.id !== $cuadroMaquina.id){
+        perder();
+        return; 
+    }
+    if(secuenciaUsuario.length === secuenciaMaquina.length){
+        bloquearClickUsuario()
+        setTimeout(manejarRonda,1000)
+    }
+    
+};
+
+function actualizarNumeroRonda(ronda){
+    const $ronda = document.querySelector('#ronda');
+    $ronda.textContent=ronda;
+};
+
+function actualizarBarraEstado(estado, error=false){
+    const $estado = document.querySelector('#estado')
+    $estado.textContent = estado 
+
+    if (error){
+        $estado.className.remove ='alert-primary'
+        $estado.className.add='alert-danger'
+    }else {
+        $estado.className.remove ='alert-danger'
+        $estado.className.add='alert-primary'
+    }
+}
 function resaltar($cuadro){
     $cuadro.style.opacity =1
     setTimeout(function(){
@@ -24,12 +86,24 @@ function resaltar($cuadro){
 
 function obtenerCuadroAleatorio() {
     const $cuadros = document.querySelectorAll('.cuadro');
-    const Indice= Math.floor(Math.random() * ($cuadros.length));
+    const indice= Math.floor(Math.random() * ($cuadros.length));
    
-   return console.log($cuadros[indice]);
+   return $cuadros[indice];
   };
-function reiniciarPartida(){
-    secuenciaMaquina=[];
-    secuenciaUsuario=[];
-    ronda=0
-};
+
+function DesbloquearClickUsuario(){
+    document.querySelectorAll('.cuadro').forEach(function($cuadro){
+        $cuadro.onclick = manejarInputUsuario
+    });
+}
+
+function bloquearClickUsuario(){
+    document.querySelectorAll('.cuadro').forEach(function($cuadro){
+    })
+}
+function perder(){
+    actualizarBarraEstado('Perdiste!!!!!')
+    setTimeout(function(){
+        alert('vuelve a intentarlo')
+    },2000)
+}
